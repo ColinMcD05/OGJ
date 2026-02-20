39b6c0a64ed65e70a01bdfd0969b82a1a7247fec
ColinMcD05 <colinmcd05@gmail.com>
2026-02-20 13:04:20 -0500

    Patrol Script

--- Assets/Colin/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Colin/Scripts/Enemy/EnemyMovement.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
+    [SerializeField] GameObject eyes;
+
     [SerializeField] Transform[] waypoints;
-    public int currentWayPointTarget = 0;
+    public int currentWaypointTarget = 0;
 
     public float moveSpeed = 4;
+    public float reachDistance = 0.1f;
     public float waitTime = 3;
+    private int moveDirection = 1;
 
     [HideInInspector] public bool sawPlayer;
     [HideInInspector] public bool seePlayer;
@@ -36,7 +40,24 @@ public class EnemyMovement : MonoBehaviour
             return;
         }
 
-        Transform target = waypoints[currentWayPointTarget];
+        Transform target = waypoints[currentWaypointTarget];
+
+        Vector2 direction = (target.position - transform.position).normalized;
+        if(direction != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            eyes.transform.rotation = targetRotation;
+        }
+        transform.position = Vector2.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+        if ((transform.position - target.position).sqrMagnitude < reachDistance * reachDistance)
+        {
+            if (currentWaypointTarget == waypoints.Length - 1)
+            {
+                moveDirection *= -1;
+            }
+            currentWaypointTarget = (currentWaypointTarget + 1 * moveDirection);
+            waitTime = Random.Range(1f, 3f);
+        }
     }
 
     void LookForPlayer()
