70666692ee2429d26a9117a913004e9ce7256b38
ColinMcD05 <colinmcd05@gmail.com>
2026-02-21 14:31:47 -0500

    Update Shadow.cs

--- Assets/Colin/Scripts/Enviorment/Shadow.cs
+++ Assets/Colin/Scripts/Enviorment/Shadow.cs
@@ -1,12 +1,16 @@
+using System;
 using UnityEngine;
 
 public class Shadow : MonoBehaviour
 {
+    [Range(0, 1)] public float fade;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerController>().inShadow = true;
+            collision.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, fade);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -14,6 +18,7 @@ public class Shadow : MonoBehaviour
         if (collision.CompareTag("Player"))
         {
             collision.GetComponent<PlayerController>().inShadow = false;
+            collision.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, 1f);
         }
     }
 }
