76d7248227d63065a0c125db682eaf12218d7642
ColinMcD05 <colinmcd05@gmail.com>
2026-02-21 21:55:17 -0500

    Collectable

---
+++ Assets/Colin/Scripts/Collectables/Collectable.cs
@@ -0,0 +1,22 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    public int collectableScore;
+    GameManager gameManager;
+
+    void Awake()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            gameManager.AddScore(collectableScore);
+            Destroy(this.gameObject);
+        }
+    }
+}
