12469be08e745a8d6a7d09776ac294042790de6a
ColinMcD05 <colinmcd05@gmail.com>
2026-02-22 15:17:58 -0500

    Update CameraMovement.cs

--- Assets/Colin/Scripts/Camera/CameraMovement.cs
+++ Assets/Colin/Scripts/Camera/CameraMovement.cs
@@ -26,7 +26,7 @@ public class CameraMovement : MonoBehaviour
         Debug.Log(Random.Range(1, 3));
         if (player.GetComponent<PlayerMovement>().playerDir == Vector2.zero)
         {
-            Camera.main.transform.position = player.transform.position;
+            Camera.main.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
         }
     }
 
