8988087cf2a77857d9c5efcf7e9d708c816611af
ColinMcD05 <colinmcd05@gmail.com>
2026-03-16 02:46:30 -0400

    fix

--- Assets/Colin/Scripts/Transition/Play.cs
+++ Assets/Colin/Scripts/Transition/Play.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Play : MonoBehaviour
 {
+    [SerializeField] EventSystem eventSystem;
+    [SerializeField] GameObject firstButton;
+
+    void Awake()
+    {
+        eventSystem.firstSelectedGameObject = firstButton;
+    }
+
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
