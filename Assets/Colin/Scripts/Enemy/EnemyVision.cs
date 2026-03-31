0af8455085a1ae62b0e6b96fac625c388f01abdb
ColinMcD05 <colinmcd05@gmail.com>
2026-03-30 21:30:08 -0400

    Vision cones set up working

--- Assets/Colin/Scripts/Enemy/EnemyVision.cs
+++ Assets/Colin/Scripts/Enemy/EnemyVision.cs
@@ -1,10 +1,12 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyVision : MonoBehaviour
 {
     [SerializeField] EnemyMovement enemyMovement;
 
+    LayerMask wallMask;
     LayerMask layerMask;
     LayerMask playerMask;
     GameObject player;
@@ -12,12 +14,21 @@ public class EnemyVision : MonoBehaviour
     public static event Action<bool> enemySees;
     [Range(1,360)] public float detectionAngle = 45;
     private PlayerController playerController;
+    public float meshRelosution;
+
+    public MeshFilter viewMeshFilter;
+    Mesh viewMesh;
 
     void Awake()
     {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+
         player = GameObject.Find("Player");
         playerMask = LayerMask.GetMask("Player");
         layerMask = LayerMask.GetMask("Player", "Wall");
+        wallMask = LayerMask.GetMask("Wall");
     }
 
     void Update()
@@ -42,6 +53,7 @@ public class EnemyVision : MonoBehaviour
         }
         Debug.Log(CheckInAngle());
         Debug.Log(CheckIsNotHidden());
+        DrawFOV();
     }
 
     bool CheckInAngle()
@@ -78,7 +90,59 @@ public class EnemyVision : MonoBehaviour
         return player.GetComponent<PlayerController>().inShadow;
     }
 
-    /*private void OnDrawGizmos()
+    void DrawFOV()
+    {
+        int stepCount = Mathf.RoundToInt(detectionAngle * meshRelosution);
+        float stepAngleSize = detectionAngle / stepCount;
+
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = - detectionAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+        }
+
+        int vertexCount = viewPoints.Count;
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangle = new int[(vertexCount - 2) * 3];
+
+        vertices[0] = Vector3.zero;
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]);
+
+            if (i < vertexCount - 2)
+            {
+                triangle[i * 3] = 0;
+                triangle[i * 3 + 1] = i + 1;
+                triangle[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangle;
+        viewMesh.RecalculateNormals();
+    }
+
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 direction = DirectionFromAngle(-transform.eulerAngles.z, globalAngle);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, visionRange, wallMask);
+
+        if (hit.collider != null)
+        {
+            return new ViewCastInfo(true, hit.point, hit.distance, globalAngle);
+        }
+        else
+        {
+            Debug.Log("Debug");
+            return new ViewCastInfo(false, transform.position + direction * visionRange, visionRange, globalAngle);
+        }
+    }
+
+    private void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
         UnityEditor.Handles.DrawWireDisc(transform.position, Vector3.forward, visionRange);
@@ -89,7 +153,7 @@ public class EnemyVision : MonoBehaviour
         Gizmos.color = Color.red;
         Gizmos.DrawLine(transform.position, transform.position + angleOne * visionRange);
         Gizmos.DrawLine(transform.position, transform.position + angleTwo * visionRange);
-    }*/
+    }
 
     private Vector2 DirectionFromAngle(float eulerY, float angleInDegrees)
     {
@@ -104,5 +168,21 @@ public class EnemyVision : MonoBehaviour
 
         Quaternion targetRotation = Quaternion.LookRotation(transform.forward, (target.position - transform.position));
         transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, (enemyMovement.rotationSpeed + 2)  * Time.deltaTime);
+    }
+
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float distance;
+        public float angle;
+
+        public ViewCastInfo (bool hit, Vector3 point, float distance, float angle)
+        {
+            this.hit = hit;
+            this.point = point;
+            this.distance = distance;
+            this.angle = angle;
+        }
     }
 }
